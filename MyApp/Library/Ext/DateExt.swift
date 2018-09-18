697c27b06d9d7fa8f6c0faf4099b2c2721952966
vanle57 <hongvan.571996@gmail.com>
2018-09-18 13:56:58 +0700

    Edited auto layout blog cell

--- MyApp/Library/Ext/DateExt.swift
+++ MyApp/Library/Ext/DateExt.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension Date {
-    func string(withFormat format: String = "dd/MM/yyyy HH:mm") -> String {
+    func string(withFormat format: String = "dd/MM/yyyy") -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
         return dateFormatter.string(from: self)
--- MyApp/Library/Helper/DummyData.swift
+++ MyApp/Library/Helper/DummyData.swift
@@ -10,7 +10,9 @@ import Foundation
 
 final class DummyData {
     static func fetchEntries() -> [Entry] {
-        let entry = Entry(imageUrl: "", title: "Mindfulness", description: "Mindfulness’ is a word we hear a lot these days, but what exactly does it mean? Find out what it is, what the benefits of mindfulness are and how you can start to practise it.", authorName: "Kirk", content: "", dateCreated: Date())
-        return [entry, entry, entry, entry]
+        let entry1 = Entry(imageUrl: "", title: "Mindfulness", description: "Mindfulness’ is a word we hear a lot these days, but what exactly does it mean? Find out what it is, what the benefits of mindfulness are and how you can start to practise it.", authorName: "Kirk Clean", content: "", dateCreated: Date())
+
+        let entry2 = Entry(imageUrl: "", title: "Mindfulness", description: "Mindfulness’ is a word we hear a lot these days, but what exactly does it mean?", authorName: "Kirk Clean", content: "", dateCreated: Date())
+        return [entry1, entry1, entry1, entry2]
     }
 }
