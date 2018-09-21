d82942bf4a2fd33379f0bea637202fbb4a078238
vanle57 <hongvan.571996@gmail.com>
2018-09-21 09:44:25 +0700

    Edited image

---
+++ MyApp/View/Controller/Cell/HistorySearchCell/HistorySearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  HistorySearchCell.swift
+//  MyApp
+//
+//  Created by MBA0002 on 9/21/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class HistorySearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
