ada5feb1adbcaa60af03b0f7b51495ec7ac630d3
vanle57 <hongvan.571996@gmail.com>
2018-09-24 11:55:43 +0700

    Added header view

---
+++ MyApp/View/Controller/HomeViewController/HeaderView/HeaderViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderViewModel.swift
+//  MyApp
+//
+//  Created by MBA0002 on 9/24/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import Foundation
+
+class HeaderViewModel {
+    var title = ""
+
+    init(title: String) {
+        self.title = title
+    }
+}
