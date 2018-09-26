35a1b27bb4232caed206e8298e7632cee8d53a96
vanle57 <hongvan.571996@gmail.com>
2018-09-26 14:01:33 +0700

    Setup teacher collection cell

---
+++ MyApp/View/Cell/TeacherTableCell/TeacherCollectionCell/TeacherCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  TeacherCollectionCell.swift
+//  MyApp
+//
+//  Created by MBA0002 on 9/26/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class TeacherCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
