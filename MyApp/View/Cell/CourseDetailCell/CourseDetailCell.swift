f1cf3f38ca27221dd59bb20a582b7894f69f398d
vanle57 <hongvan.571996@gmail.com>
2018-09-28 08:08:48 +0700

    Added course detail cell

---
+++ MyApp/View/Cell/CourseDetailCell/CourseDetailCell.swift
@@ -0,0 +1,21 @@
+//
+//  CourseDetailCell.swift
+//  MyApp
+//
+//  Created by MBA0002 on 9/27/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class CourseDetailCell: UITableViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
