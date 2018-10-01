23e9e8291a8ddb70ba2dfceeff91c1aea8c7e78f
vanle57 <hongvan.571996@gmail.com>
2018-10-01 11:34:05 +0700

    Fixed convention

---
+++ MyApp/Library/Ext/UIViewExt.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewExt.swift
+//  MyApp
+//
+//  Created by MBA0002 on 10/1/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import Foundation
--- MyApp/View/Controller/CourseDetailViewController/CourseDetailViewController.swift
+++ MyApp/View/Controller/CourseDetailViewController/CourseDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CourseDetailViewController: UIViewController {
+final class CourseDetailViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
