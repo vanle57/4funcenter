1e8f89050a64a277db10adf60bc77d86eb2d0c7c
vanle57 <hongvan.571996@gmail.com>
2018-10-01 09:38:36 +0700

    Added course comment cell

---
+++ MyApp/View/Cell/CourseCommentCell/CourseCommentCellViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  CourseCommentCellViewModel.swift
+//  MyApp
+//
+//  Created by MBA0203 on 9/29/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+final class CourseCommentCellViewModel {
+    
+    // MARK: - Propeties
+    var avatar: UIImage = UIImage()
+    var username = "demi lovato"
+    var content = ""
+    var dateCreate = ""
+    var ratingPoint = 0
+    
+    var ratingString: String {
+        switch ratingPoint {
+        case 1:
+            return "★"
+        case 2:
+            return "★★"
+        case 3:
+            return "★★★"
+        case 4:
+            return "★★★★"
+        case 5:
+            return "★★★★★"
+        default:
+            return ""
+        }
+    }
+    
+    // MARK: - init
+    init(comment: Comment) {
+//        self.avatar = comment.user.
+        content = comment.content
+        dateCreate = comment.dateCreate.string()
+        ratingPoint = comment.ratingPoint
+    }
+    
+}
