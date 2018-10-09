b2362c73b0175c5b7dd721a13b0dfdc10523272e
vanle57 <hongvan.571996@gmail.com>
2018-10-09 10:11:36 +0700

    Added object class

--- MyApp/Library/Helper/DummyData.swift
+++ MyApp/Library/Helper/DummyData.swift
@@ -52,4 +52,10 @@ final class DummyData {
         let comment2 = Comment(content: "Through a combination of lectures, comprehension and vocabulary quizzes, practice and performance, you’ll gain the skills and confidence to communicate well  in English, anywhere in the world.", ratingPoint: 3, dateCreate: Date())
         return [comment1, comment2, comment2, comment1]
     }
+
+    static func fetchClasses() -> [CourseClass] {
+        let class1 = CourseClass(name: "BC 01", scheduler: "2-4-6 7h30:9h00")
+        let class2 = CourseClass(name: "BC 02", scheduler: "3-5-7 7h30:9h00")
+        return [class1, class2]
+    }
 }
---
+++ MyApp/Model/Schema/Class.swift
@@ -0,0 +1,9 @@
+//
+//  Class.swift
+//  MyApp
+//
+//  Created by MBA0002 on 10/9/18.
+//  Copyright © 2018 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import Foundation
